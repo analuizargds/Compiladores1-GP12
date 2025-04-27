9670ff0a84d704995018a66bbda069bff8848d37
Lucas Meireles <lucas.meireles@example.com>
2025-04-27 20:26:01 -0300

    loops

---
+++ testes/loopBC.c
@@ -0,0 +1,10 @@
+for(int i=0; i<30; i=i+1){
+    if(i== 12){
+        continue;
+    }
+
+    if(i == 10)
+        break;
+    
+
+}
---
+++ testes/loopTipos.c
@@ -0,0 +1,12 @@
+for(int i=0; i==10;i=i + 1){
+
+}
+
+do{
+    i=i+1;
+}while (i<10);
+
+
+while(i<20){
+    i=i+1;
+}
