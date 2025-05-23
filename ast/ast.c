93ddddb2255b27497e68245c1ba2f9e8f5664032
Wonz Lopes <112433653+WonnzDA@users.noreply.github.com>
2025-05-23 14:51:46 -0300

    Update ast.c

--- ast/ast.c
+++ ast/ast.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ast.h"
+
+
 ASTNode *noSwitch(ASTNode *expr, ASTNode *cases) {
     ASTNode *no = criarNo(AST_SWITCH, NULL);
     no->filho1 = expr;
