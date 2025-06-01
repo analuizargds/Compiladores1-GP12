7896e40d8fe5170ec0d51448a69759c6952cb67c
Lucas Meireles <lucas.meireles@example.com>
2025-05-31 23:06:20 -0300

    refactor: refactor simbolos

--- simbolos.h
+++ simbolos.h
@@ -1,11 +1,19 @@
 #ifndef SIMBOLOS_H
 #define SIMBOLOS_H
 
-// adaptar quando tiver a AST
+#include "ast/ast.h"
+
 typedef enum
 {
     TIPO_INT,
     TIPO_FLOAT,
+    TIPO_CHAR,
+    TIPO_VOID,
+    TIPO_DOUBLE,
+    TIPO_STRUCT,
+    TIPO_UNION,
+    TIPO_ENUM,
+    TIPO_FUNCAO,
     TIPO_ERRO
 } TipoSimbolo;
 
@@ -17,6 +25,7 @@ typedef struct simbolo
     char nome[TAM_NOME];
     TipoSimbolo tipo;
     int nivel;
+    ASTNode *ast_node;
     struct simbolo *proximo;
 } Simbolo;
 
